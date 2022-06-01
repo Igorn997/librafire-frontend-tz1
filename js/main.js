8d98e71ae3ea8ff04a168ad3b38035997e0205a1
Igorn997 <igor97nikolic@gmail.com>
2022-06-01 11:08:06 +0200

    Navigacija za tablete i telefone zavrsena

--- js/main.js
+++ js/main.js
@@ -30,13 +30,33 @@ $(".center").slick({
   ],
 });
 
-const nav = document.querySelector(".header__main-nav_link");
+// // const nav = document.querySelector(".header__main-nav_link");
 
-// const open = document.querySelectorAll(".nav-active");
+// // // const open = document.querySelectorAll(".nav-active");
 
-const openActive = function () {
-  nav.classList.add("nav-active");
+// // const openActive = function () {
+// //   nav.classList.add("nav-active");
+// //   console.log("clicked");
+// // };
+
+// // document.addEventListener("click", openActive);
+
+const btnNavEL = document.querySelector(".btn-mobile-nav");
+const HeaderEL = document.querySelector(".header__main-nav");
+const btnMenu = document.querySelector(".icon-mobile-nav-menu");
+const btnX = document.querySelector(".icon-mobile-nav-x");
+
+btnNavEL.addEventListener("click", function () {
+  HeaderEL.classList.toggle("nav-open");
+});
+
+btnMenu.addEventListener("click", () => {
   console.log("clicked");
-};
+  btnMenu.style.display = "none";
+  btnX.style.display = "block";
+});
 
-document.addEventListener("click", openActive);
+btnX.addEventListener("click", () => {
+  btnX.style.display = "none";
+  btnMenu.style.display = "block";
+});
